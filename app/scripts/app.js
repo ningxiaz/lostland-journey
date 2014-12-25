0853c25148d6928b736ad453ef43d364ed636a98
Ningxia Zhang <ningxia@apropose.com>
2014-12-24 16:33:29 -0800

    artist daily counts

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,6 @@
 // starting point of the app
 
-journey.data.getLastfmTracks('thmmrs2298');
+journey.data.getLastfmArtistTracks('thmmrs2298', 'Kate Bush', function(data) {
+    var counts = journey.data.computeArtistDailyCounts(data.artisttracks.track);
+    console.log(counts);
+});
--- app/scripts/data.js
+++ app/scripts/data.js
@@ -6,6 +6,8 @@ journey.data = (function() {
     var lastfmKey = 'bd286e68d3aa369779ff55dfe15470b6';
 
     var getLastfmTracks = function(username) {
+        console.log('getting ' + username + '\'s tracks!');
+
         $.ajax({
             url: lastfmBase + '?method=user.getrecenttracks&user=' + username + '&api_key=' + lastfmKey + '&format=json',
             success: function(data) {
@@ -17,11 +19,51 @@ journey.data = (function() {
                 }
             }
         });
-        console.log('getting ' + username + '\'s tracks!');
+        
+    };
+
+    var getLastfmArtistTracks = function(username, artist, callback) {
+        console.log('getting ' + username + '\'s tracks by ' + artist + '!');
+
+        $.ajax({
+            url: lastfmBase + '?method=user.getArtistTracks&user=' + username + '&artist=' + artist + '&api_key=' + lastfmKey + '&format=json',
+            success: function(data) {
+                if (data.error) {
+                    console.log('ERROR [data.getLastfmArtistTracks]:' + data.message);
+                }
+                else {
+                    console.log(data);
+                    if(callback) {
+                        callback(data);
+                    }
+                }
+            }
+        });
+    };
+
+    var computeArtistDailyCounts = function(tracks) {
+        var counts = {};
+        tracks.forEach(function(track) {
+            var dateObj = new Date(parseInt(track.date.uts) * 1000);
+            console.log(track.date['#text']);
+            // day string will be something like "2014 3 23" (April 23, 2014) local time
+            var dayString = dateObj.getFullYear() + ' ' + dateObj.getMonth() + ' ' + dateObj.getDate();
+
+            if(!counts.hasOwnProperty(dayString)) {
+                counts[dayString] = 1;
+            }
+            else {
+                counts[dayString] += 1;
+            }
+        });
+
+        return counts;
     };
 
     return {
-        getLastfmTracks: getLastfmTracks
+        getLastfmTracks: getLastfmTracks,
+        getLastfmArtistTracks: getLastfmArtistTracks,
+        computeArtistDailyCounts: computeArtistDailyCounts
     };
 })();
 
