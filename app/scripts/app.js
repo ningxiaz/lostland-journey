376b3d646e3c0ea20b470a931239d31147d26399
Ningxia Zhang <ningxia@apropose.com>
2014-12-25 15:39:05 -0800

    daily counts for all artists over a period of time

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,12 @@
 // starting point of the app
 
-journey.data.getLastfmArtistTracks('thmmrs2298', 'Kate Bush', function(data) {
-    var counts = journey.data.computeArtistDailyCounts(data.artisttracks.track);
-    console.log(counts);
+
+journey.data.getLastfmTracks('thmmrs2298', new Date(2014, 11, 1), new Date(2014, 11, 23), function(data) {
+    console.log(journey.data.computeDailyCounts(data));
 });
+
+
+// journey.data.getLastfmArtistTracks('thmmrs2298', 'Kate Bush', function(data) {
+//     var counts = journey.data.computeArtistDailyCounts(data.artisttracks.track);
+//     console.log(counts);
+// });
--- app/scripts/data.js
+++ app/scripts/data.js
@@ -5,21 +5,48 @@ journey.data = (function() {
     var lastfmBase = 'http://ws.audioscrobbler.com/2.0/';
     var lastfmKey = 'bd286e68d3aa369779ff55dfe15470b6';
 
-    var getLastfmTracks = function(username) {
+    // from, to are both Date objects
+    var getLastfmTracks = function(username, from, to, callback) {
         console.log('getting ' + username + '\'s tracks!');
 
-        $.ajax({
-            url: lastfmBase + '?method=user.getrecenttracks&user=' + username + '&api_key=' + lastfmKey + '&format=json',
-            success: function(data) {
-                if (data.error) {
-                    console.log('ERROR [data.getLastfmTracks]:' + data.message);
+        var fromParam = from.getTime() / 1000;
+        var toParam = to.getTime() / 1000;
+
+        var allData = [];
+
+        // a recursive helper function to get all pages of data
+        function recentTracks (currentPage, totalPages) {
+            if(currentPage === totalPages) {
+                console.log(allData);
+                if(callback) {
+                    callback(allData);
                 }
-                else {
-                    console.log(data);
-                }
+                return;
             }
-        });
-        
+
+            currentPage ++;
+
+            $.ajax({
+                url: lastfmBase + '?method=user.getrecenttracks&user=' + username + '&api_key=' + lastfmKey + '&limit=200&format=json&from=' + fromParam + '&to=' + toParam + '&page=' + currentPage,
+                success: function(data) {
+                    if (data.error) {
+                        console.log('ERROR [data.getLastfmTracks]:' + data.message);
+                    }
+                    else {
+                        var total = parseInt(data.recenttracks['@attr'].totalPages);
+                        var current = parseInt(data.recenttracks['@attr'].page);
+                        console.log('Current Page: ' + current);
+                        console.log('Total Page: ' + total);
+                        allData = allData.concat(data.recenttracks.track);
+                        recentTracks(current, total);
+                    }
+                }
+            });
+
+        }
+
+        // trigger the recursive function to get data
+        recentTracks(0, null);
     };
 
     var getLastfmArtistTracks = function(username, artist, callback) {
@@ -60,10 +87,36 @@ journey.data = (function() {
         return counts;
     };
 
+    var computeDailyCounts = function(tracks) {
+        var counts = {};
+        tracks.forEach(function(track) {
+            var artistName = track.artist['#text'];
+            if(!counts.hasOwnProperty(artistName)) {
+                counts[artistName] = {};
+            }
+            // a new track might not have date field
+            if(track.date) {
+                var dateObj = new Date(parseInt(track.date.uts) * 1000);
+                // day string will be something like "2014 3 23" (April 23, 2014) local time
+                var dayString = dateObj.getFullYear() + ' ' + dateObj.getMonth() + ' ' + dateObj.getDate();
+
+                if(!counts[artistName].hasOwnProperty(dayString)) {
+                    counts[artistName][dayString] = 1;
+                }
+                else {
+                    counts[artistName][dayString] += 1;
+                }
+            }
+        });
+
+        return counts;
+    };
+
     return {
         getLastfmTracks: getLastfmTracks,
         getLastfmArtistTracks: getLastfmArtistTracks,
-        computeArtistDailyCounts: computeArtistDailyCounts
+        computeArtistDailyCounts: computeArtistDailyCounts,
+        computeDailyCounts: computeDailyCounts
     };
 })();
 
